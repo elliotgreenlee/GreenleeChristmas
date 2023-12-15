4566ed4b6b8a443fd3601cba983890e1c1f4969d
Elliot Greenlee <elliotgreenlee@gmail.com>
2023-12-15 13:11:41 -0600

    Update to 2023 fields and archive 2021.

--- pages/christmas2021.py
+++ pages/christmas2021.py
@@ -6,5 +6,5 @@ log = logging.getLogger('gunicorn.error')
 
 
 def index():
-    gifts2023 = Gift.query.filter_by(year='2023').all()
-    return render_template('index.html', gifts=gifts2023)
+    gifts2021 = Gift.query.filter_by(year='2021').all()
+    return render_template('index.html', gifts=gifts2021)
